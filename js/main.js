1d533e8d438088ebc9bcbf4c9d07426a0d207afa
D4ZZL3R <nnitnmotwaani002@gmail.com>
2020-01-28 17:37:25 +0530

    Dependent Select V1.1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,40 @@
-{/* <script src ="/js/jquery.js"></script> */}
-// debugger;
-console.log("HELLO");
-var coun =  new Array;
+var conVal, cityVal;
+var coun = new Array;
 var obj1 = new XMLHttpRequest();
 var url = "/data/csc.json";
-obj1.onreadystatechange = function ()
-{
-    if(obj1.readyState == 4 && obj1.status == 200)
-    {
-        // debugger;
-        var jsondata = JSON.parse(obj1.responseText);
-            coun = jsondata;
-            // debugger;
 
+obj1.onreadystatechange = function () {
+    if (obj1.readyState == 4 && obj1.status == 200) {
+        var jsondata = JSON.parse(obj1.responseText);
+        coun = jsondata;
     }
 };
-obj1.open("GET",url,true);
+obj1.open("GET", url, true);
 obj1.send();
+
+$(document).ready(function () {
+    $("#country option").hide();
+    for (var i = 0; i < coun.length; i++) {
+        $("#country").append("<option value =" + coun[i].Name + ">" + coun[i].Name + "</option>");
+    }
+
+    $("#country").change(function () {
+        $("#city option").hide();
+        conVal = $(this).val();
+        for (var i = 0; i < coun.length; i++) {
+            if (conVal == coun[i].Name) {
+                for (var j = 0; j < coun[i].States.length; j++) {
+                    $("#city").append("<option value =" + coun[i].States[j] + ">" + coun[i].States[j] + "</option>");
+                }
+            }
+        }
+        $("#city option:first").prop("selected", true);
+    });
+
+    $("#city").change(function () {
+        cityVal = $(this).val();
+        var rr = "<tr><td>" + conVal + "</td><td>" + cityVal + "</td></tr>";
+        $("#tab tr:last").after(rr);
+
+    });
+});
